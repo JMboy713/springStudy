abec8949556fde8ccbc8895fe825baa611115f0c
Jmboy713 <jimin713@naver.com>
2024-01-19 16:31:49 +0900

    0119

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package hello.hellospring.repository;
+import hello.hellospring.domain.Member;
+import java.util.Optional;
+import java.util.List;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findByID(Long id);// optional : null 일 경우 처리
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
