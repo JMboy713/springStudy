5047cc353c84c0f6f57265e05a26847b07593e91
Jmboy713 <jimin713@naver.com>
2024-02-15 17:43:00 +0900

    AOP

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -23,15 +23,22 @@ public class MemberService {
     }
     /*회원가입 */
     public Long join(Member member) {
+//        long start = System.currentTimeMillis();
         // 같은 이름이 있는 중복 회원 X
 //        Optional<Member> result = memberRepository.findByName(member.getName()); // optional
 //        result.ifPresent(m->{ // 값이 있으면?
 //            throw new IllegalStateException("이미 존재하는 회원입니다.");
 //        });
+//        try{
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+//        }finally{
+//            long finish = System.currentTimeMillis();
+//            long timeMs = finish - start;
+//            System.out.println("join = "+timeMs+"ms");
+//        }
 
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
